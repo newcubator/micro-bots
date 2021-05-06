af74d0e5c697a757600971dacfdf52990f98e139
Jonathan Zbick <jonathan.zbick@newcubator.com>
2021-05-06 16:22:42 +0200

    add lock/unlock project bot

---
+++ src/lock-project/createSlackResponses.ts
@@ -0,0 +1,20 @@
+const responseTemplate = (text) => ({
+    response_type: 'ephemeral',
+    text
+});
+
+export const lockUnlockProjectErrorNoProjectId = () => {
+    return responseTemplate('Du hast keine Projekt ID angegeben oder deine Projekt ID war ungültig. Versuche das Kommando noch einmal mit einer korrekten Projekt ID aufzurufen.');
+};
+
+export const lockUnlockProjectErrorNoProjectFound = () => {
+    return responseTemplate('Es wurde zu der angegebenen Projekt ID kein Projekt gefunden.');
+};
+
+export const lockProjectSuccess = (projectId: string) => {
+    return responseTemplate(`Ich habe das Projekt mit der ID: ${projectId} erfolgreich gesperrt.`);
+};
+
+export const unlockProjectSuccess = (projectId: string) => {
+    return responseTemplate(`Ich habe das Projekt mit der ID: ${projectId} erfolgreich entsperrt.`);
+};
