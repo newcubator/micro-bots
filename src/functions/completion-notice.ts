262f9af0710c522f9f68cd3ab4d8ab2d8d3b4440
Sven Röttering <sven.roettering@newcubator.com>
2021-06-10 16:25:34 +0200

    Debug performance issues in CompletionNotice function

--- src/functions/completion-notice.ts
+++ src/functions/completion-notice.ts
@@ -17,6 +17,7 @@ import { slackUploadFileToChannel } from '../slack/slack';
 import { SlackCommandType } from '../slack/types/slack-types';
 
 export const handler = async (event: APIGatewayEvent) => {
+    console.time('Completion Notice');
     const command: SlackCommandType = decode(event.body) as SlackCommandType;
     const orderNumber = command.text?.trim();
 
@@ -25,30 +26,41 @@ export const handler = async (event: APIGatewayEvent) => {
     }
     sendEphemeralResponse(command.response_url, completionNoticeSuccess(orderNumber));
 
+    console.time('Fetching Projects');
     const project = (await getProjects()).find(project => project.custom_properties.Bestellnummer === orderNumber);
+    console.timeEnd('Fetching Projects');
     console.log(`project: ${project}`);
     if (project === undefined) {
         return completionNoticeErrorNoProjectFound();
     }
 
+    console.time('Fetching Deal');
     const deal = (await getDealById(project.deal.id));
+    console.timeEnd('Fetching Deal');
     console.log(`deal: ${deal}`);
 
     if (deal === undefined) {
         return completionNoticeErrorNoDealFound();
     }
 
+    console.time('Fetching Contact');
     const contact = (await getContactById(deal.person.id));
+    console.timeEnd('Fetching Contact');
 
     if (contact === undefined) {
         return completionNoticeErrorNoContactFound();
     }
 
+    console.time('Creating PDF');
     const pdf: jsPDF = createCompletionNoticePdf(project, contact);
+    console.timeEnd('Creating PDF');
 
+    console.time('Slack Upload');
     await slackUploadFileToChannel(process.env.COMPLETION_NOTICE_CHANNEL,
         Buffer.from(pdf.output('arraybuffer')),
         `${orderNumber}.pdf`,
         `Hier ist die Fertigstellungsanzeige für das Projekt: ${project.name}, Bestellnummer: ${orderNumber}`
     );
+    console.timeEnd('Slack Upload');
+    console.timeEnd('Completion Notice');
 };
