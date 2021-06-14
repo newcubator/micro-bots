653d3752fc44d337f151dfa229da5334b4ef2d0e
Jan Sauer <jan.sauer@newcubator.com>
2021-06-14 14:16:22 +0200

    Switch to esbuild
    Makes building and testing faster. Also the configuration is much simpler.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 process.env.SLACK_TOKEN = "i dont really need this token";
 
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "esbuild-jest"
+  },
 };
