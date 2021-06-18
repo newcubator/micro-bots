9ac80339093e8379e760de0a2c7468e2925a7d94
Sven Röttering <sven.roettering@newcubator.com>
2021-06-18 16:16:22 +0200

    Revert "Remove timing logs from completion notice lambda"
    This reverts commit 550643af

--- src/functions/completion-notice.ts
+++ src/functions/completion-notice.ts
@@ -1,19 +1,7 @@
 import { APIGatewayEvent } from 'aws-lambda';
-import { jsPDF } from 'jspdf';
+import { Lambda } from 'aws-sdk';
 import { decode } from 'querystring';
-import { createCompletionNoticePdf } from '../completion-notice/createCompletionNoticePdf';
-import {
-    completionNoticeErrorNoContactFound,
-    completionNoticeErrorNoDealFound,
-    completionNoticeErrorNoOrderNumber,
-    completionNoticeErrorNoProjectFound,
-    completionNoticeSuccess
-} from '../completion-notice/createSlackResponses';
-import { sendEphemeralResponse } from '../completion-notice/sendEphemeralResponse';
-import { getContactById } from '../moco/contacts';
-import { getDealById } from '../moco/deals';
-import { getProjects } from '../moco/projects';
-import { slackUploadFileToChannel } from '../slack/slack';
+import { completionNoticeErrorNoOrderNumber } from '../completion-notice/createSlackResponses';
 import { SlackCommandType } from '../slack/types/slack-types';
 
 export const handler = async (event: APIGatewayEvent) => {
@@ -23,32 +11,28 @@ export const handler = async (event: APIGatewayEvent) => {
     if (orderNumber === undefined) {
         return completionNoticeErrorNoOrderNumber();
     }
-    sendEphemeralResponse(command.response_url, completionNoticeSuccess(orderNumber));
 
-    const project = (await getProjects()).find(project => project.custom_properties.Bestellnummer === orderNumber);
-    console.log(`project: ${project}`);
-    if (project === undefined) {
-        return completionNoticeErrorNoProjectFound();
-    }
+    const lambda = new Lambda({
+        region: 'eu-central-1',
+    });
 
-    const deal = (await getDealById(project.deal.id));
-    console.log(`deal: ${deal}`);
+    lambda.invoke({
+        FunctionName: 'micro-bots-production-completionNoticePdfCreation',
+        InvocationType: 'Event',
+        LogType: 'Tail',
+        Payload: JSON.stringify({ orderNumber }),
+    },
+        (error, data) => {
+            if (error) {
+                console.error(error);
+            }
+            if (data) {
+                console.log(data);
+            }
+        });
 
-    if (deal === undefined) {
-        return completionNoticeErrorNoDealFound();
-    }
-
-    const contact = (await getContactById(deal.person.id));
-
-    if (contact === undefined) {
-        return completionNoticeErrorNoContactFound();
-    }
-
-    const pdf: jsPDF = createCompletionNoticePdf(project, contact);
-
-    await slackUploadFileToChannel(process.env.COMPLETION_NOTICE_CHANNEL,
-        Buffer.from(pdf.output('arraybuffer')),
-        `${orderNumber}.pdf`,
-        `Hier ist die Fertigstellungsanzeige für das Projekt: ${project.name}, Bestellnummer: ${orderNumber}`
-    );
+    return {
+        statusCode: 200,
+        body: `Danke für deine Anfrage, ich werde nach einem Projekt mit der Bestellnummer: "${orderNumber}" suchen.`,
+    };
 };
