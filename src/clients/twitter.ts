8af23571b635a434c95a4afa7ca8b6cbfea7307b
Lars Silberg <lars.silberg@newcubator.com>
2021-10-18 15:46:13 +0200

    fixed formating

--- src/clients/twitter.ts
+++ src/clients/twitter.ts
@@ -1,9 +1,8 @@
-import { TwitterApi } from 'twitter-api-v2';
+import { TwitterApi } from "twitter-api-v2";
 
 export const twitterClient = new TwitterApi({
-    appKey: process.env.TWITTER_APP_KEY,
-    appSecret: process.env.TWITTER_APP_SECRET,
-    accessToken: process.env.TWITTER_ACCESS_TOKEN,
-    accessSecret: process.env.TWITTER_ACCESS_SECRET,
+  appKey: process.env.TWITTER_APP_KEY,
+  appSecret: process.env.TWITTER_APP_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_SECRET,
 });
-
--- src/functions/twitter-bot.spec.ts
+++ src/functions/twitter-bot.spec.ts
@@ -1,17 +1,15 @@
-import {handler} from './twitter-bot';
-import Parser from 'rss-parser';
-import { TwitterApi } from 'twitter-api-v2';
+import { handler } from "./twitter-bot";
+import Parser from "rss-parser";
+import { TwitterApi } from "twitter-api-v2";
 
-jest.mock('twitter-api-v2');
-jest.mock('rss-parser');
+jest.mock("twitter-api-v2");
+jest.mock("rss-parser");
 
-const TwitterMock = TwitterApi as jest.MockedClass<typeof TwitterApi>
-const ParserMock = Parser as jest.MockedClass<typeof Parser>
+const TwitterMock = TwitterApi as jest.MockedClass<typeof TwitterApi>;
+const ParserMock = Parser as jest.MockedClass<typeof Parser>;
 
-
-test('handler', async () => {
-    await expect(handler()).resolves
-    expect(ParserMock.prototype.parseURL).toHaveBeenCalledWith('https://newcubator.com/devsquad/rss.xml')
-    expect(TwitterMock.prototype.v2.userByUsername).toHaveBeenCalledWith('newcubator')
+test("handler", async () => {
+  await expect(handler()).resolves;
+  expect(ParserMock.prototype.parseURL).toHaveBeenCalledWith("https://newcubator.com/devsquad/rss.xml");
+  expect(TwitterMock.prototype.v2.userByUsername).toHaveBeenCalledWith("newcubator");
 });
-
