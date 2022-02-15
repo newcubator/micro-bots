fb42c0b86d21e33aea6d49f8f5212b1c64e78bd0
Lars Silberg <lars.silberg@newcubator.com>
2022-02-15 12:05:06 +0100

    prevent the bot from running in the dev environment

--- src/functions/twitter-bot.ts
+++ src/functions/twitter-bot.ts
@@ -17,11 +17,13 @@ export const handler = async () => {
       const plannedTweet = composeTweetFromPost(notYetTweeted[0]);
       console.info(`Planned Tweeting ${plannedTweet.guid}: ${plannedTweet.message}`);
 
-      await saveTweetedPost(googleSheetsAccessor, plannedTweet);
-      console.info("Saved new Tweet.");
+      if (process.env.AWS_LAMBDA_FUNCTION_NAME?.includes("production")) {
+        await saveTweetedPost(googleSheetsAccessor, plannedTweet);
+        console.info("Saved new Tweet.");
 
-      const createdTweet = await twitterClient.v1.tweet(plannedTweet.message);
-      console.info(`Tweeted ${createdTweet.id_str}: ${createdTweet.full_text}`);
+        const createdTweet = await twitterClient.v1.tweet(plannedTweet.message);
+        console.info(`Tweeted ${createdTweet.id_str}: ${createdTweet.full_text}`);
+      }
     }
   } catch (err) {
     console.error(err);
