35c52fcb5643607c2cd9591262ad83b6538c4cbb
Sophia Brandt <1578703-sophiabrandt@users.noreply.gitlab.com>
2022-02-21 10:24:23 +0000

    apply short form

--- src/slack/interaction-handler.ts
+++ src/slack/interaction-handler.ts
@@ -37,7 +37,7 @@ export const interactionHandler = async (event: APIGatewayEvent) => {
           responseUrl: blockAction.response_url,
           messageTs: blockAction.container.message_ts,
           channelId: blockAction.container.channel_id,
-          actionType: actionType,
+          actionType,
         });
       case ActionType.COMPLETION_NOTICE:
         return new CompletionNoticeRequestedEvent({
