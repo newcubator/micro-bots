7769a36d3a22ec1457b0a636ebcf9df79040730e
Lars Silberg <lars.silberg@newcubator.com>
2022-08-17 09:27:27 +0200

    add test for hannover sender location

--- src/short-mail/event-handler.spec.ts
+++ src/short-mail/event-handler.spec.ts
@@ -21,8 +21,7 @@ const renderShortMailPdfMock = renderShortMailPdf as jest.Mock;
 const conversationsJoinMock = slackClient.conversations.join as jest.Mock;
 const fileUploadMock = slackClient.files.upload as jest.Mock;
 const axiosPostMock = axios.post as jest.Mock;
-
-test("handle event short mail generation", async () => {
+test("handle event short mail generation dortmund to male with company adress", async () => {
   getContactByIdMock.mockResolvedValueOnce({
     id: 1,
     gender: "H",
@@ -90,5 +89,68 @@ test("handle event short mail generation", async () => {
   expect(axiosPostMock).toHaveBeenCalledWith("https://slack.com/response_url", {
     replace_original: "true",
     text: expect.stringContaining("Bill Gates"),
+  });
+});
+
+test("handle event short mail generation hannover to female without company adress", async () => {
+  getContactByIdMock.mockResolvedValueOnce({
+    id: 2,
+    gender: "F",
+    firstname: "Melinda",
+    lastname: "Gates",
+    work_address: "\nFensterstraße 1\n12345 Fensterhausen ",
+    company: null,
+  });
+  getCompanieMock.mockResolvedValueOnce({});
+
+  renderShortMailPdfMock.mockResolvedValueOnce(Buffer.from("pdf"));
+
+  getSlackNameMock.mockResolvedValueOnce({
+    ok: true,
+    profile: {
+      real_name: "Max Mustermann",
+    },
+  });
+
+  await eventHandler({
+    detail: {
+      responseUrl: "https://slack.com/response_url",
+      personId: "2",
+      message: "Testnachricht an Melinda",
+      sender: "1337",
+      messageTs: "1633540187.000600",
+      channelId: "C02BBA8DWVD",
+      location: "H",
+    },
+  } as any);
+
+  expect(getCompanieMock).toBeCalledTimes(1);
+  expect(getSlackNameMock).toHaveBeenCalledWith("1337");
+  expect(getContactByIdMock).toHaveBeenCalledWith("2");
+  expect(renderShortMailPdfMock).toHaveBeenCalledWith({
+    sender: "Max Mustermann",
+    senderAdressHeader: "newcubator GmbH | Bödekerstraße 22 | 30161 Hannover",
+    senderAdressFooter: "\nBödekerstraße 22\n30161 Hannover\n+49 (0) 511-95731300\n",
+    recipient: {
+      salutation: "geehrte Frau",
+      firstname: "Melinda",
+      lastname: "Gates",
+      address: "\nFensterstraße 1\n12345 Fensterhausen ",
+    },
+    date: dayjs(),
+    text: "Testnachricht an Melinda",
+  });
+  expect(conversationsJoinMock).toHaveBeenCalledWith({ channel: "C02BBA8DWVD" });
+  expect(fileUploadMock).toHaveBeenCalledWith({
+    file: expect.anything(),
+    filename: "Kurzbrief Gates.pdf",
+    initial_comment: "",
+    channels: "C02BBA8DWVD",
+    thread_ts: "1633540187.000600",
+    broadcast: "true",
+  });
+  expect(axiosPostMock).toHaveBeenCalledWith("https://slack.com/response_url", {
+    replace_original: "true",
+    text: expect.stringContaining("Melinda Gates"),
   });
 });
