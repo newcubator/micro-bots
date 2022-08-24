01dcbe5669623120e4716f46769908842bde27fc
Lars Silberg <lars.silberg@newcubator.com>
2022-08-24 08:25:53 +0000

    Update mail signature generator url

--- src/functions/mail-signature.ts
+++ src/functions/mail-signature.ts
@@ -11,7 +11,7 @@ export const handler = async (event: APIGatewayEvent) => {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: `Du kannst deine Mail Signatur unter https://2ktbbx9mpf.execute-api.eu-central-1.amazonaws.com/production/mailSignatureGenerator?user_id=${command.user_id}&user_name=${command.user_name} abrufen.`,
+          text: `Du kannst deine Mail Signatur unter https://kceiclszhf.execute-api.eu-central-1.amazonaws.com/production/mailSignatureGenerator?user_id=${command.user_id}&user_name=${command.user_name} abrufen.`,
         },
       },
     ],
