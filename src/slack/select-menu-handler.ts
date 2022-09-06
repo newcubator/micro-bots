04ccdd879cc203b547dd7a454237e985c92d945a
Lars Silberg <lars.silberg@newcubator.com>
2022-09-06 12:11:42 +0200

    fix filter contacts

--- src/slack/select-menu-handler.ts
+++ src/slack/select-menu-handler.ts
@@ -1,6 +1,6 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { decode } from "querystring";
-import { getAllContacts, getContacts } from "../moco/contacts";
+import { getAllContacts } from "../moco/contacts";
 import { MocoContact, MocoUserType } from "../moco/types/moco-types";
 import { getUsers } from "../moco/users";
 import { BlockSuggestion } from "./types/slack-types";
@@ -8,14 +8,13 @@ import { BlockSuggestion } from "./types/slack-types";
 let loadAllRecipients: Promise<any> = initAllRecipients();
 
 export const selectMenuHandler = async (event: APIGatewayEvent) => {
-  // const blockSuggestion: BlockSuggestion = JSON.parse(decode(event.body).payload as string) as BlockSuggestion;
-  // console.log(blockSuggestion);
+  const blockSuggestion: BlockSuggestion = JSON.parse(decode(event.body).payload as string) as BlockSuggestion;
+  console.log(blockSuggestion);
 
   const recipients = await loadAllRecipients;
-
+    console.log(recipients)
   const filteredContacts = recipients.filter((person) =>
-    // person.text.text.includes(blockSuggestion.value.toLowerCase())
-    person.text.text.includes("jan")
+    person.text.text.toLowerCase().includes(blockSuggestion.value.toLowerCase())
   );
   console.log(JSON.stringify(filteredContacts));
 
