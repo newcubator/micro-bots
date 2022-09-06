cbc090dacd3e6ab6ab2cf6c554185c47b1f919f5
Lars Silberg <lars.silberg@newcubator.com>
2022-09-06 11:16:35 +0200

    add pagination

--- src/moco/contacts.ts
+++ src/moco/contacts.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { autoPage } from "./auto-page";
 import { MOCO_TOKEN } from "./token";
 import { MocoContact } from "./types/moco-types";
 
@@ -20,4 +21,16 @@ export async function getContacts() {
       },
     })
     .then((response) => response.data);
+}
+
+const getContactsPaged = (page: number) =>
+  axios.get<any>(`https://newcubator.mocoapp.com/api/v1/contacts/people/`, {
+    headers: {
+      Authorization: "Token token=" + MOCO_TOKEN,
+    },
+    params: { page },
+  });
+
+export async function getAllContacts() {
+  return autoPage<MocoContact>((page: number) => getContactsPaged(page));
 }
--- src/slack/select-menu-handler.ts
+++ src/slack/select-menu-handler.ts
@@ -1,6 +1,6 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { decode } from "querystring";
-import { getContacts } from "../moco/contacts";
+import { getAllContacts, getContacts } from "../moco/contacts";
 import { getUsers } from "../moco/users";
 import { BlockSuggestion } from "./types/slack-types";
 
@@ -11,7 +11,8 @@ export const selectMenuHandler = async (event: APIGatewayEvent) => {
   const blockSuggestion: BlockSuggestion = JSON.parse(decode(event.body).payload as string) as BlockSuggestion;
   console.log(blockSuggestion);
 
-  contacts = await getContacts();
+  contacts = await getAllContacts();
+  console.log("Anzahl:" + contacts.length);
   users = await getUsers();
   const people = contacts.concat(users);
 
