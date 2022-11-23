a789369aa75ec56f6a22df9428d71478d7556b69
patrickschaper <patrickschaper@chapie.de>
2022-11-23 10:17:23 +0100

    fix dates for Tests

--- src/functions/holiday-set-automatic-mail-replies.spec.ts
+++ src/functions/holiday-set-automatic-mail-replies.spec.ts
@@ -1,44 +1,79 @@
-import axios from "axios";
+import dayjs from "dayjs";
+import { changeMailRespondForUser } from "../microsoft/mail";
+import { getUsers as microsoftGetUsers } from "../microsoft/users";
 import { getUsersWithVacation } from "../moco/vacation";
 import { handler } from "./holiday-set-automatic-mail-replies";
+import { slackChatPostMessage } from "../slack/slack";
 
 jest.mock("../moco/vacation");
+jest.mock("../microsoft/users");
+jest.mock("../microsoft/mail");
+jest.mock("../slack/slack");
 
 const mockGetUsersWithVacation = getUsersWithVacation as unknown as jest.Mock;
-const axiosGetMock = axios.get as jest.Mock;
+const mockMicrosoftGetUsers = microsoftGetUsers as unknown as jest.Mock;
+const mockMicrosoftChangeMailRespondForUser = changeMailRespondForUser as unknown as jest.Mock;
+const mockSlackChatPostMessage = slackChatPostMessage as unknown as jest.Mock;
 
 describe("HolidaySetAutomaticMailReplies", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-    process.env.MICROSOFT_TOKEN = "1be0afda-f723-4ad4-bdc6-7603041ebe0e";
-    process.env.MICROSOFT_CLIENT_ID = "5b846559-5861-42d4-92b0-787bef833831";
-    process.env.MICROSOFT_CLIENT_SECRET = "JzB8Q~gcjTIBR8MR2lYDGfgd73slHsgidO6H.czI";
-  });
   it("should set a mail reply for user", async () => {
+    const oldHoliday = dayjs().subtract(3, "day");
+    const startHoliday = dayjs().add(1, "day");
+    const endHoliday = dayjs().add(4, "day");
+    const availableDate = dayjs().add(5, "day").format("DD.MM.YYYY");
+
     mockGetUsersWithVacation.mockResolvedValueOnce([
       {
         user: {
           email: "MaxMustermann@email.de",
+          custom_properties: {
+            Standort: "Dortmund",
+          },
         },
-        dates: ["2022-11-08", "2022-11-10", "2022-11-14"],
+        dates: [oldHoliday.format("YYYY-MM-DD"), startHoliday.format("YYYY-MM-DD"), endHoliday.format("YYYY-MM-DD")],
         employment: null,
       },
     ]);
-    axiosGetMock.mockResolvedValueOnce({
-      access_token: "fake token",
-    });
-    axiosGetMock.mockResolvedValueOnce({
-      values: [
+
+    mockMicrosoftGetUsers.mockResolvedValueOnce({
+      value: [
         {
           mail: "MaxMustermann@email.de",
           id: "1234-5678-9012",
+          displayName: "Max Mustermann",
         },
       ],
     });
-    axiosGetMock.mockResolvedValueOnce({});
+
+    mockMicrosoftChangeMailRespondForUser.mockResolvedValueOnce({});
+    mockSlackChatPostMessage.mockResolvedValueOnce({});
+
+    await expect(handler()).resolves.toBeUndefined();
+
+    expect(mockMicrosoftChangeMailRespondForUser.mock.calls.length).toBe(1);
+    expect(mockMicrosoftChangeMailRespondForUser.mock.calls[0]).toEqual([
+      "1234-5678-9012",
+      '<html><body><div style="font-family:Calibri,Arial,Helvetica,sans-serif; font-size:12pt; color:rgb(0,0,0)">\n    Guten Tag,\n\n    Vielen Dank für Ihre Nachricht. Ich befinde mich aktuell im Urlaub und habe währenddessen keinen Zugriff auf meine Mails. \n    Aus Vertraulichkeitsgründen wird Ihre E-Mail nicht automatisch weitergeleitet. Ich bin ab dem ' +
+        availableDate +
+        ' wieder verfügbar. Bei dringenden Anliegen wenden Sie sich bitte an unser Büro unter \n    (<a href="mailto:info@newcubator.com">info@newcubator.com</a> / 0231-58687380)\n\n    Vielen Dank!\n\n    Mit freundlichen Grüßen\n\n    Max Mustermann\n    </div></body></html>',
+      startHoliday.format("YYYY-MM-DD"),
+      endHoliday.format("YYYY-MM-DD"),
+    ]);
+    expect(mockSlackChatPostMessage.mock.calls.length).toEqual(1);
+    expect(mockSlackChatPostMessage.mock.calls[0]).toEqual([
+      "Die E-Mail Response für Max Mustermann wurde gesetzt. Hab einen schönen Urlaub!",
+      "1111111",
+      "Mail Bot",
+      ":e-mail:",
+    ]);
+  });
+
+  it("should return a useful message if nobody has holiday", async () => {
+    mockGetUsersWithVacation.mockResolvedValueOnce([]);
 
     const result = await handler();
 
-    expect(result).toBe(1);
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toEqual("Aktuell hat niemand Urlaub");
   });
 });
