6d977e36f2b4b134d971de3d96de10831231c901
Lars Silberg <lars.silberg@newcubator.com>
2023-06-13 11:48:27 +0200

    delete twitterbot, instead generate blog article with gitlab issue

--- src/functions/devsquad-to-blog-generator.ts
+++ src/functions/devsquad-to-blog-generator.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const handler = async () => {
   const response = await axios.get(`https://newcubator.com/api/devsquad-ai?secret=${process.env.DEVSQUAD_AI_SECRET}`);
-    console.info("Response from Devsquad AI", response.data);
-}
+  console.info("Response from Devsquad AI", response.data);
+};
