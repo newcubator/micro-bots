20f5e090da5e6c89110eaad5f575431848653477
Lars Silberg <lars.silberg@newcubator.com>
2023-06-19 10:12:59 +0200

    delete twitterbot docs; delete devsquad cron job

--- src/functions/devsquad-to-blog-generator.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const handler = async () => {
-  const response = await axios.get(`https://newcubator.com/api/devsquad-ai?secret=${process.env.DEVSQUAD_AI_SECRET}`);
-  console.info("Response from Devsquad AI", response.data);
-};
